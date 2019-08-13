ef0222e6eaf6c42692f719657cceaf29fbbfbbe1
gaoshudian <18818196720@163.com>
2019-08-13 22:08:33 +0800

    处理默认标签中的自定义标签元素

--- src/main/java/com/zhuguang/jack/customtag/CustomtagTest.java
+++ src/main/java/com/zhuguang/jack/customtag/CustomtagTest.java
@@ -4,10 +4,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import redis.clients.jedis.Jedis;
 
+/**
+ * 自定义标签测试
+ */
 public class CustomtagTest {
     
     public static void main(String[] args) {
-        ApplicationContext app = new ClassPathXmlApplicationContext("mytest.xml");
+        ApplicationContext app = new ClassPathXmlApplicationContext("customtag.xml");
         System.out.println(app);
 
         Jedis client1 = (Jedis)app.getBean("redis");
--- src/main/java/com/zhuguang/jack/customtag/MyBeanDefinitionDecorator.java
+++ src/main/java/com/zhuguang/jack/customtag/MyBeanDefinitionDecorator.java
@@ -2,28 +2,32 @@ package com.zhuguang.jack.customtag;
 
 import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.xml.BeanDefinitionDecorator;
-import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
 import org.springframework.beans.factory.xml.ParserContext;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
-public class TagsNamespaceHandler extends NamespaceHandlerSupport {
+/**
+ * @Description: 自定义装饰器，主要应用这种配置情况：
 
-    public void init() {
-        this.registerBeanDefinitionParser("mongo", new MongoBeanDifinitionParser());
-        this.registerBeanDefinitionParser("redis", new RedisBeanDifinitionParser());
-    }
-
-    /*
     这里的bean使用的是默认的标签配置，但是其中的子元素却使用了自定义的配置
-        对应源码:delegate.decorateBeanDefinitionIfRequired(ele, bdHolder);
-
     <bean id="man" class="com.zhuguang.jack.entity.lookup_method.Man">
         <jack:redis id="redis" ip="127.0.0.1" port="6379"></jack:redis>
     </bean>
-     */
+    对应源码:delegate.decorateBeanDefinitionIfRequired(ele, bdHolder);
+
+    其实这里就是把默认标签解析出来的BeanDefinitionHolder进一步装饰（修改）
+
+ * @Auther: gaoshudian
+ * @Date: 2019/8/13 21:56
+ */
+public class MyBeanDefinitionDecorator implements BeanDefinitionDecorator{
     @Override
     public BeanDefinitionHolder decorate(Node node, BeanDefinitionHolder definition, ParserContext parserContext) {
-        registerBeanDefinitionDecorator("myBeanDefinitionDecorator", new MyBeanDefinitionDecorator());
-        return super.decorate(node, definition, parserContext);
+        //自定义装饰操作
+        String ip = ((Element)node).getAttribute("ip");
+        String port = ((Element)node).getAttribute("port");
+        //其它操作
+        //......
+        return definition;
     }
 }
